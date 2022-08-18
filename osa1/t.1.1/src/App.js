ad1bc360d4a7ae33d0ae48bddf9fa74b9593201a
Viivi Mustonen <viivimustonen@Viivi-MacBook-Air.local>
2022-08-18 09:36:11 +0300

    t.1.2 step2

--- osa1/t.1.1/src/App.js
+++ osa1/t.1.1/src/App.js
@@ -28,12 +28,23 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
+      <Part part1 = {part1} exercises1 = {exercises1} />
+      <Part part2 = {part2} exercises2 = {exercises2} />
+      <Part part3 = {part3} exercises3 = {exercises3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <div>
       <p> {props.part1} {props.exercises1} </p>
       <p> {props.part2} {props.exercises2} </p>
       <p> {props.part3} {props.exercises3} </p>
     </div>
   )
 }
+
 // Total tehtävien yhteismäärästä
 const Total = (props) => {
   return (
