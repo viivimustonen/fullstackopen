0399d0cad28ba049a6ba06f5cdf534f9cb4e8d6d
Viivi Mustonen <viivimustonen@Viivi-MacBook-Air.local>
2022-08-20 11:45:00 +0300

    t.1.6 step1

---
+++ osa1/t.1.6/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseByOneGood = () => setGood(good + 1)
+  const increaseByOneNeutral = () => setNeutral(neutral + 1)
+  const increaseByOneBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1> Give feedback </h1>
+      
+      <Button
+        handleClick={increaseByOneGood}
+        text='good'
+      />
+      <Button
+        handleClick={increaseByOneNeutral}
+        text='neutral'
+      />
+      <Button
+        handleClick={increaseByOneBad}
+        text='bad'
+      />
+
+      <h1> Statistics </h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+export default App
+
