fde5d90d819ebbfe9dfc847789191966080e7f21
Viivi Mustonen <viivimustonen@Viivi-MacBook-Air.local>
2022-08-20 12:51:00 +0300

    t.1.9 step4

--- osa1/t.1.6/src/App.js
+++ osa1/t.1.6/src/App.js
@@ -1,17 +1,20 @@
 import { useState } from 'react'
 
 const Statistics = (props) => {
-  return (
-    <div>
-    <h1> Statistics </h1>
-        <p>good {props.good}</p>
-        <p>neutral {props.neutral}</p>
-        <p>bad {props.bad}</p>
-        <p>all {props.good + props.neutral + props.bad}</p>
-        <p>average {(props.good*1 + props.neutral * 0 + props.bad * (-1))/(props.good + props.neutral + props.bad)}</p>
-        <p>positive {(props.good/(props.good + props.neutral + props.bad))*100}%</p>
-    </div>
-  )
+  if (props.good == 0 && props.neutral == 0 && props.bad == 0) {
+    return <p>No feedback given</p>
+  } else{
+    return (
+      <div>
+          <p>good {props.good}</p>
+          <p>neutral {props.neutral}</p>
+          <p>bad {props.bad}</p>
+          <p>all {props.good + props.neutral + props.bad}</p>
+          <p>average {(props.good*1 + props.neutral * 0 + props.bad * (-1))/(props.good + props.neutral + props.bad)}</p>
+          <p>positive {(props.good/(props.good + props.neutral + props.bad))*100}%</p>
+      </div>
+    )   
+  }
 }
 
 const App = () => {
@@ -19,8 +22,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  let allClicks = 0;
 
   const increaseByOneGood = () => setGood(good + 1)
   const increaseByOneNeutral = () => setNeutral(neutral + 1)
@@ -42,7 +43,8 @@ const App = () => {
         handleClick={increaseByOneBad}
         text='bad'
       />
-      <Statistics good = {good} setGood ={setGood} neutral = {neutral} setNeutral = {setNeutral} bad = {bad} setBad = {setBad} allClicks = {allClicks}></Statistics>
+      <h1> Statistics </h1>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
     
   )
@@ -57,4 +59,3 @@ const Button = (props) => {
 }
 
 export default App
-
