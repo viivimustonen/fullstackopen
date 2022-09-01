5d02af151c33336d400b501ff441e80bb000a4fd
Viivi Mustonen <viivimustonen@Viivi-MacBook-Air.local>
2022-09-01 13:33:49 +0300

    t.2.1 step6

---
+++ osa2/t.2.1/src/App.js
@@ -0,0 +1,62 @@
+
+const Course = ({ course }) => (
+  <div>
+   <Header course={course} />
+   <Content course={course} />
+  </div>
+)
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((part) => (<Part key={part.id} part={part} />))}
+    </div>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>    
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
+
